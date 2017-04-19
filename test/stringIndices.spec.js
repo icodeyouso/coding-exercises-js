7c2c8a20aece9ed9845a469e82073606c889bd08
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-04-18 17:11:53 -0700

    pushed

--- test/stringIndices.spec.js
+++ test/stringIndices.spec.js
@@ -6,15 +6,16 @@ console.log("hello", typeof(stringIndices))
 
 describe('stringIndices()', () => {
     it('should exsist', () => {
-        return expect(stringIndices).to.be.a('function')
+        expect(stringIndices).to.be.a('function')
     })
 
     it('return word at given index number', () => {
-        return expect(stringIndices(2, 'cash me outside')).to.equal('me')
+            expect(stringIndices)(2, 'cash me outside')).to.equal('me')
     })
 
-    it('your program should return an empty string if index is smaller or larger than stringArrays', () => {
+it('your program should return an empty string if index is smaller or larger than stringArrays', () => {
+    expect(stringIndices)()
 
-    })
+})
 
 })
