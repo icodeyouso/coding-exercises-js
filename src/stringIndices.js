a0489e61024a9ee96b14c6b3f6f93079debd2456
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-04-19 10:38:16 -0700

    push

--- src/stringIndices.js
+++ src/stringIndices.js
@@ -1,19 +1,38 @@
-const readlineSync = require('readline-sync');
-
-const user = [];
-const splitString = "cash me outside".split
-const indexOf = stringArray[index]
-const stringArray = ["boy", "girl"];
+const stringArray = ["boy", "girl"]
 const stringIndex = function(index) {
     // take the users input 
     // compare to my array 
     //return the value of that index
 
-    for (var i = 0; i < stringIndex.length; i++) {
-        if (userInput === stringIndex[1])
-            return stringIndex[i]
-    }
+
+    return stringArray[index - 1]
 }
+stringIndex(1);
 
 
-//return stringIndex();
+
+
+
+
+
+
+
+// const readlineSync = require('readline-sync');
+
+// const user = [];
+// const splitString = "cash me outside".split
+// const indexOf = stringArray[index]
+// const stringArray = ["boy", "girl"];
+// const stringIndex = function(index) {
+//     // take the users input 
+//     // compare to my array 
+//     //return the value of that index
+
+//     for (var i = 0; i < stringIndex.length; i++) {
+//         if (userInput === stringIndex[1])
+//             return stringIndex[i]
+//     }
+// }
+
+
+// //return stringIndex();
