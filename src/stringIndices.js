a3b2e1f9dc8f112b548f5e9b3d3cca02f8d3a67f
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-04-18 17:00:30 -0700

    new push

---
+++ src/stringIndices.js
@@ -0,0 +1,15 @@
+const readlineSync = require('readline-sync');
+
+const index = 2
+const splitString = "cash me outside".split
+const indexOf = stringArray[index]
+const stringArray = ["boy", "girl"];
+const stringIndex = function(index) {
+    // take the users input 
+    // compare to my array 
+    //return the value of that index
+    if (index > indexOf) {
+
+    }
+    //return stringIndex();
+}
