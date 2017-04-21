4ef5b44589f37a65fca74470a10d04a7c94c884e
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-04-21 13:28:36 -0700

    changes to editor config

---
+++ src/TheDisemvowing.js
@@ -0,0 +1,21 @@
+var removeVowels_1 = function(text) {
+        var result = "";
+        for (var i = 0; i < text.length; i++) {
+            var c = text[i];
+            if ("aeiou".match(c.tolowerCase()) == null {
+                    result += c
+                }
+            }
+            return result
+
+            var removeVowels_2 = function(text) {
+                    var result = ""
+                    result = text.where(c => !"aeiou".any(x => x == c.toLowerCase()));
+                    return result;
+                    string.prototype.where = function(predictable) {
+                            string.prototype.any = function(predicatable) {
+
+                            }
+                            var removeVowels_3 = function(text) {
+                                run();
+                            }
